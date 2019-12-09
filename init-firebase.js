cc729dec58c166593a44dffff825c35af4a28df8
David Barrow <davec@MSI-DEV.DAVE>
2019-12-09 03:36:32 +0000

    Initaial commit

---
+++ init-firebase.js
@@ -0,0 +1,14 @@
+//Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBvAjlCwTdFhbef65IKyGowQ95m4zYxohI",
+  authDomain: "journey-man-1572045507787.firebaseapp.com",
+  databaseURL: "https://journey-man-1572045507787.firebaseio.com",
+  projectId: "journey-man-1572045507787",
+  storageBucket: "journey-man-1572045507787.appspot.com",
+  messagingSenderId: "717838517050",
+  appId: "1:717838517050:web:02a523e39d4550615a0603",
+  measurementId: "G-5BZY6DVJEB"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
