cb0acdfd6c086640640c4d1838b058596859e346
David Barrow <davec@MSI-DEV.DAVE>
2019-12-09 05:58:41 +0000

    last vuefire snippet

---
+++ public/read_data.vue
@@ -0,0 +1,16 @@
+// retrieve a collection
+db.collection('documents')
+  .get()
+  .then(querySnapshot => {
+    const documents = querySnapshot.docs.map(doc => doc.data())
+    // do something with documents
+  })
+
+// retrieve a document
+db.collection('documents')
+  .doc(documentId)
+  .get()
+  .then(snapshot => {
+    const document = snapshot.data()
+    // do something with document
+  })
